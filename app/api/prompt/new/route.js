f15fdce6a8bbf6d378d2eaef27bd42779ee4f561
sarthakvermaa18 <sarthakverma2313@gmail.com>
2024-06-03 12:55:07 +0530

    first commit

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,34 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const POST = async (req) => {
+  const { userId, prompt, tag } = await req.json();
+  try {
+
+    await connectToDB();
+
+    const newPrompt = new Prompt({
+      creator: userId,
+      prompt,
+      tag,
+    });
+
+    await newPrompt.save();
+
+    return new Response(JSON.stringify(newPrompt), {
+      status: 201,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error("Error creating prompt:", error);
+
+    return new Response(JSON.stringify({ error: "Failed to create a new prompt" }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+};
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      appDir: true,
+      serverComponentsExternalPackages: ["mongoose"],
+    },
+    images: {
+      domains: ['lh3.googleusercontent.com'],  // Corrected to 'domains' and fixed the domain value
+    },
+    webpack(config) {
+      config.experiments = {
+        ...config.experiments,
+        topLevelAwait: true,
+      };
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
